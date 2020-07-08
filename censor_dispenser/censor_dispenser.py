8d4a1fce16d33b59bf017eab249f06f77281d869
Justin Ward <justin@quietlike.ninja>
2020-07-08 17:22:36 +1000

    Censor functions fail to replace capitalized words Fixes #1

--- censor_dispenser/censor_dispenser.py
+++ censor_dispenser/censor_dispenser.py
@@ -5,13 +5,15 @@ email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
 
 def censor_text(text,censor):
-    censored_text = ""
+    censored_mask = ""
     for i in range(len(censor)):
         if censor[i] == " ":
-            censored_text = censored_text + " "
+            censored_mask = censored_mask + " "
         else:
-            censored_text = censored_text + "X"
-    return text.replace(censor,censored_text)
+            censored_mask = censored_mask + "X"
+    censored_text = text.replace(censor,censored_mask)
+    censored_text = censored_text.replace(censor.capitalize(),censored_mask)
+    return censored_text
 
 def censor_list_of_text(text,censor):
     for entry in censor:
