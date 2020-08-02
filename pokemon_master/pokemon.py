f768d6fa167033c08b7aab560d5ca7980cb02277
Justin Ward <justin@quietlike.ninja>
2020-08-03 00:27:55 +1000

    Change pokemon definitions and started on Trainer

--- pokemon_master/pokemon.py
+++ pokemon_master/pokemon.py
@@ -2,11 +2,21 @@ class Pokemon:
     def __init__(self, name, level, type):
         self.name = name
         self.level = level
-        self.type = type
-        self.max_health = level * 100
+        self.type = type.capitalize()
+        self.max_health = level * 5
         self.health = self.max_health
         self.is_knocked_out = False
     
+    def __repr__(self):
+        return f'''
+        Name:        {self.name}
+        Level:       {self.level}
+        Type:        {self.type}
+        Max Health:  {self.max_health}
+        Cur Health:  {self.health}
+        Knocked out: {self.is_knocked_out}
+        '''
+
     def lose_health(self, points):
         if self.health - points <= 0:
             self.knock_out()
@@ -40,7 +50,6 @@ class Pokemon:
             print(f"{self.name} doesn't need to be revived")
 
     def attack(self, other_pokemon):
-        attack_multiplier = 1
         print(f"{self.name} attacks {other_pokemon.name}")
         if self.is_knocked_out:
             print(f"{self.name} can't attack while knocked out")
@@ -48,30 +57,93 @@ class Pokemon:
         if other_pokemon.is_knocked_out:
             print(f"{other_pokemon.name} is already knocked out")
             return
-        if attack_types.get(self.type):
-            if other_pokemon.type in attack_types.get(self.type).get("advantage"):
-                attack_multiplier = 2
-                print(f"{self.name} has an advantage of a {attack_multiplier} times attack multiplier")
+        if attack_types.get(self.type.lower()):
+            if other_pokemon.type.lower() in attack_types.get(self.type.lower()).get("advantage"):
+                print(f"{self.name} has attacked {other_pokemon.name} with an attack multiplier of 2 dealing {self.level * 2} damage")
+                other_pokemon.lose_health(self.level * 2)
+            elif other_pokemon.type.lower() in attack_types.get(self.type.lower()).get("disadvantage"):
+                print(f"{self.name} has attacked {other_pokemon.name} with an attack multiplier of 0.5 dealing {self.level * 0.5} damage")
+                other_pokemon.lose_health(self.level * 0.5)
             else:
-                attack_multiplier = 0.5
-                print(f"{self.name} has a disadvantage of a {attack_multiplier} times attack multiplier")
+                print(f"{self.name} has attacked {other_pokemon.name} with no attack multiplier dealing {self.level} damage")
+                other_pokemon.lose_health(self.level)
         else:
+            #Should this be an exception?
             print("No type in DB")
-        other_pokemon.lose_health(10 * attack_multiplier)
-        
+
+class Charmander(Pokemon):
+    def __init__(self, level = 1):
+        super().__init__("Charmander", level, "fire")
+
+class Squirtle(Pokemon):
+    def __init__(self, level = 1):
+        super().__init__("Squirtle", level, "water")
+
+class Gloom(Pokemon):
+    def __init__(self, level = 1):
+        super().__init__("Gloom", level, "grass")
+
+class Trainer:
+    def __init__(self, name, pokemons, num_potions, active_pokemon = 0):
+        self.name = name
+        self.pokemons = pokemons
+        self.num_potions = num_potions
+        self.active_pokemon = active_pokemon
+
+    def __repr__(self):
+        print(f'''
+        Trainer Name:   {self.name}
+        Potions:        {self.num_potions}
+        Active Pokemon: {self.pokemons[self.active_pokemon].name}
+        ''')
+        for pokemon in self.pokemons:
+            if pokemon.name == self.pokemons[self.active_pokemon].name:
+                print('''
+        *****Active*****''')
+            print(pokemon)
+        return ""
+    
+    def set_active(self, pokemon):
+        #add logic to cater for non existent pokemon and knoocked out pokemon
+        self.active_pokemon = pokemon
+        print(f'{self.name}\'s current pokemon is now {self.pokemons[self.active_pokemon].name}')
+
+    def get_active(self):
+        print(f'{self.name}\'s current pokemon is {self.pokemons[self.active_pokemon].name}')
+
+    def heal(self):
+        #add logic to heal active pokemon
+    
+    def attack(self, other_trainer):
+        #add logic for active pokemon to attack other trainers active pokemon
+
+
 attack_types = {
     "fire": {"advantage": ["grass"], "disadvantage": ["fire", "water"]},
     "water": {"advantage": ["fire"], "disadvantage": ["grass", "water"]},
     "grass": {"advantage": ["water"], "disadvantage": ["fire", "grass"]}
 }
-charmander = Pokemon("Charmander", 1, "fire")
-squirtle = Pokemon("Squirtle", 1, "water")
-gloom = Pokemon("Gloom", 1, "grass")
 
-charmander.attack(squirtle)
-charmander.attack(gloom)
+a = Charmander(5)
+b = Squirtle(2)
+c = Gloom(10)
+d = Charmander()
 
-#charmander.attack(squirtle)
+t1 = Trainer("Justin", [a,c], 6, 1)
+t2 = Trainer("Steph", [b,d], 6, 1)
+
+t1.get_active()
+t1.set_active(0)
+t1.get_active()
+
+
+"""b.attack(d)
+d.attack(b)
+b.attack(d)
+b.attack(d)
+b.attack(d)
+
+print(d)"""
 #Charmander attackes Squirtle
 #charmander.lose_health(100)
 #Charmander has been knocked out
